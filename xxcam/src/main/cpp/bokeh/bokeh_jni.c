2becba4da3fd61786516077c159b10b6ed4ed1f6
MatrixMuto <wang.qiang.mu@gmail.com>
2017-05-09 18:30:55 +0800

    prepare dual

---
+++ xxcam/src/main/cpp/bokeh/bokeh_jni.c
@@ -0,0 +1,15 @@
+
+#include <jni.h>
+#include <android/log.h>
+
+#include <sys/types.h>
+
+
+JNIEXPORT void JNICALL
+Java_com_mut0_xxcam_Native_bokeh_1process(JNIEnv *env, jclass type, jobject mainByteBuffer,
+                                          jint remaining, jint width, jint height,
+                                          jobject subByteBuffer, jint remaining1, jint width1,
+                                          jint height1) {
+
+
+}
